7fcc94e3b8f242c9086050b8f174610286d8fc55
Renato Lourinho <lourinho.renato@gmail.com>
2025-05-26 13:00:35 +0100

    feat: Configure i18n routing and Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,46 @@
 import { defineConfig } from "astro/config";
+import paraglide from "@inlang/paraglide-astro"
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // Use astro's i18n routing for deciding which language to use
+  i18n: {
+    locales: ["en", "pt"],
+    defaultLocale: "pt",
+    routing: {
+      prefixDefaultLocale: true
+    }
+},
+
+integrations: [
+    paraglide({
+        // recommended settings
+        project: "./project.inlang",
+        outdir: "./src/paraglide", //where your files should be
+    }),
+],
+
+output: 'server',
+adapter: vercel({
+  edgeMiddleware: false,
+  imageService: true,
+  devImageService: 'sharp',
+  imagesConfig: {                                                                                               
+    sizes: [400, 640, 750, 828, 1080, 1200, 1920],                                                                   
+    domains: [],                                                                                                
+    formats: ['image/webp'],                                                                                    
+    minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },                  
+}),
+});
